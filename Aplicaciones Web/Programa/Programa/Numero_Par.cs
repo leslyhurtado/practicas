18c041652de2c2ce8ea6048435491a6f3e1b55b2
leslyhurtado <mask_lhj@hotmail.com>
2017-09-10 13:37:55 -0600

    programa que define si un numero es par o impar. con variable estatica.

---
+++ Aplicaciones Web/Programa/Programa/Numero_Par.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programa
+{
+    class Numero_Par
+    {
+        public int Numero { get; set; }
+        public string MostrarNumeroPar()
+        {
+            return "El Numero: " + Numero + " es par";
+        }
+        public string MostrarNumeroNoPar()
+        {
+            return "El Numero: " + Numero + " es Impar";
+        }
+    }
+}
---
+++ Aplicaciones Web/Programa/Programa/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Numero_Par numero = new Numero_Par();
+            numero.Numero = 1;
+            if ((numero.Numero % 2 ) == 0 ) 
+            {
+                Console.WriteLine (numero.MostrarNumeroPar());
+            }
+            else
+            {
+                Console.WriteLine(numero.MostrarNumeroNoPar());
+            }
+            Console.ReadKey();
+        }
+     }
+}
